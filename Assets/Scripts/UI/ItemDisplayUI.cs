8368534141b1cd35a88003863b2cc49318f87735
Malik <malik.muhametchin@gmail.com>
2024-02-17 06:20:09 +0100

    improve inventory ui and make walls look dirty

--- Assets/Scripts/UI/ItemDisplayUI.cs
+++ Assets/Scripts/UI/ItemDisplayUI.cs
@@ -1,14 +1,17 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 public sealed class ItemDisplayUI : MonoBehaviour
 {
 
     [SerializeField] private Image _image;
+    [SerializeField] private TextMeshProUGUI _label;
 
     public void Init(Item item)
     {
         _image.sprite = item.Sprite;
+        _label.text = item.DisplayName;
     }
 
 }
