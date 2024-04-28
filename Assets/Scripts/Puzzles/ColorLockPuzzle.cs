9e75a5ebe475467a9cb90c8d016a3f672a756f65
Malik <malik.muhametchin@gmail.com>
2024-04-28 22:22:44 +0200

    implement ColorLock

--- Assets/Scripts/Puzzles/ColorLockPuzzle.cs
+++ Assets/Scripts/Puzzles/ColorLockPuzzle.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,9 +8,13 @@ public class ColorLockPuzzle : Pawn
 
     [SerializeField] private Door _door;
     [SerializeField] private ColorLockRotator[] _rotators;
+    [SerializeField] private Material _defaultMaterial;
+    [SerializeField] private Material _selectedMaterial;
     [SerializeField] private Transform _camera;
+    [SerializeField] private MoviePawn _successPawn;
 
     private int _selectedRotator;
+    private TimeUntil _timeUntilInputAvaliable;
 
     private void Start()
     {
@@ -26,14 +31,30 @@ public class ColorLockPuzzle : Pawn
         return _camera.rotation;
     }
 
+    public override void OnPossessed(Player player)
+    {
+        base.OnPossessed(player);
+        _selectedRotator = _rotators.Length - 1;
+        _rotators[_selectedRotator].SetMaterial(_selectedMaterial);
+    }
+
+    public override void OnUnpossessed()
+    {
+        base.OnUnpossessed();
+        _rotators[_selectedRotator].SetMaterial(_defaultMaterial);
+    }
+
     public override void InputTick()
     {
+        if (_timeUntilInputAvaliable > 0)
+            return;
+
         int previousSelectedRotator = _selectedRotator;
 
-        if (Input.GetKeyDown(KeyCode.A) == true)
+        if (Input.GetKeyDown(KeyCode.S) == true)
             _selectedRotator--;
 
-        if (Input.GetKeyDown(KeyCode.D) == true)
+        if (Input.GetKeyDown(KeyCode.W) == true)
             _selectedRotator++;
 
         if (_selectedRotator < 0)
@@ -42,11 +63,64 @@ public class ColorLockPuzzle : Pawn
         if (_selectedRotator == _rotators.Length)
             _selectedRotator = _rotators.Length - 1;
 
-        if (Input.GetKeyDown(KeyCode.W) == true)
+        if (Input.GetKeyDown(KeyCode.A) == true)
+        {
             _rotators[_selectedRotator].RotateUp();
+            BlockInputFor(0.2f);
+        }
 
-        if (Input.GetKeyDown(KeyCode.S) == true)
+        if (Input.GetKeyDown(KeyCode.D) == true)
+        {
             _rotators[_selectedRotator].RotateDown();
+            BlockInputFor(0.2f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F) == true)
+        {
+            TryUnlock();
+            BlockInputFor(0.3f);
+        }
+
+        if (previousSelectedRotator != _selectedRotator)
+        {
+            OnRotatorSelectionChanged(previousSelectedRotator, _selectedRotator);
+            BlockInputFor(0.2f);
+        }
+    }
+
+    private void BlockInputFor(float duration)
+    {
+        _timeUntilInputAvaliable = new TimeUntil(Time.time * duration);
+    }
+
+    private void TryUnlock()
+    {
+        bool shouldUnlock = true;
+
+        foreach (var rotator in _rotators)
+        {
+            if (rotator.IsRightDigit == true)
+                continue;
+
+            shouldUnlock = false;
+            break;
+        }
+
+        if (shouldUnlock == false)
+        {
+            _door.TryOpen();
+            return;
+        }
+
+        _door.Unblock();
+        _door.TryOpen();
+        Player.Possess(_successPawn);
+    }
+
+    private void OnRotatorSelectionChanged(int previous, int current)
+    {
+        _rotators[previous].SetMaterial(_defaultMaterial);
+        _rotators[current].SetMaterial(_selectedMaterial);
     }
 
 }
--- Assets/Scripts/Puzzles/ColorLockRotator.cs
+++ Assets/Scripts/Puzzles/ColorLockRotator.cs
@@ -3,14 +3,23 @@ using UnityEngine;
 
 public sealed class ColorLockRotator : MonoBehaviour
 {
+
+    [SerializeField] private int _requiredDigit;
+
     public int CurrentDigit { get; private set; }
+    public bool IsRightDigit => CurrentDigit == _requiredDigit;
+
+    private void Start()
+    {
+        OnDigitUpdated(false);
+    }
 
     public void RotateUp()
     {
         CurrentDigit--;
 
         if (CurrentDigit < 0)
-            CurrentDigit = 8;
+            CurrentDigit = 9;
 
         OnDigitUpdated(true);
     }
@@ -19,7 +28,7 @@ public sealed class ColorLockRotator : MonoBehaviour
     {
         CurrentDigit++;
 
-        if (CurrentDigit > 8)
+        if (CurrentDigit > 9)
             CurrentDigit = 0;
 
         OnDigitUpdated(true);
@@ -31,9 +40,15 @@ public sealed class ColorLockRotator : MonoBehaviour
         OnDigitUpdated(false);
     }
 
+    public void SetMaterial(Material material)
+    {
+        GetComponent<MeshRenderer>().sharedMaterial = material;
+    }
+
     private void OnDigitUpdated(bool animate)
     {
-        Vector3 targetEuler = new Vector3(0f, 0f, CurrentDigit * 40f);
+        float targetAngle = CurrentDigit * 36f;
+        Vector3 targetEuler = new Vector3(targetAngle, 0f, 0f);
 
         if (animate == true)
             transform.DOLocalRotate(targetEuler, 0.2f);
