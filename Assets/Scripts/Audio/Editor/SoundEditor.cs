aea8ce10d83da7a15e8b3330fe8a6152c3b3eaee
Malik <malik.muhametchin@gmail.com>
2024-04-29 06:23:17 +0200

    super actually fix sound preview

--- Assets/Scripts/Audio/Editor/SoundEditor.cs
+++ Assets/Scripts/Audio/Editor/SoundEditor.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public sealed class SoundEditor : Editor
 {
 
-    [SerializeField] private AudioSource _previewSource;
+    private static AudioSource _previewSource;
 
     public override void OnInspectorGUI()
     {
@@ -14,20 +14,32 @@ public sealed class SoundEditor : Editor
 
         if (GUILayout.Button("Play Preview") == true)
         {
-            _previewSource.transform.position = FindObjectOfType<AudioListener>().transform.position;
-            (target as Sound).Play(_previewSource);
+            PlayPreview();
         }
-    }
-
-    private void OnEnable()
-    {
-        _previewSource = new GameObject().AddComponent<AudioSource>();
-        _previewSource.gameObject.hideFlags = HideFlags.DontSave;
     }
 
     private void OnDisable()
     {
+        if (_previewSource == null)
+            return;
+
         DestroyImmediate(_previewSource.gameObject);
+    }
+
+    private void PlayPreview()
+    {
+        EnsurePreviewSource();
+        _previewSource.transform.position = FindObjectOfType<AudioListener>().transform.position;
+        (target as Sound).Play(_previewSource);
+    }
+
+    private void EnsurePreviewSource()
+    {
+        if (_previewSource != null)
+            return;
+
+        _previewSource = new GameObject().AddComponent<AudioSource>();
+        _previewSource.gameObject.hideFlags = HideFlags.HideAndDontSave;
     }
 
 }
